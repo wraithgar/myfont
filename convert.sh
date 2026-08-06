c4817310a1e822827e60dc371ba9d974aafcbfba
Gar <gar+gh@danger.computer>
2026-08-06 08:37:28 -0700

    build dir

--- convert.sh
+++ convert.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 # assumes myfont.psftx is the authoritative source
-~/Development/console-fonts-utils/psftx2psf myfont.psftx myfont.psf
-bitsnpicas convertbitmap -f ttf -o myfont.ttf myfont.psf
-bitsnpicas convertbitmap -f otb -o myfont.otb myfont.psf
+mkdir -p build
+rm -f build/*
+~/Development/console-fonts-utils/psftx2psf myfont8x16.psftx build/myfont8x16.psf
+bitsnpicas convertbitmap -f ttf -o build/myfont8x16.ttf build/myfont8x16.psf
+bitsnpicas convertbitmap -f otb -o build/myfont8x16.otb build/myfont8x16.psf
+
+~/Development/console-fonts-utils/psftx2psf myfont16x32.psftx build/myfont16x32.psf
+bitsnpicas convertbitmap -f ttf -o build/myfont16x32.ttf build/myfont16x32.psf
+bitsnpicas convertbitmap -f otb -o build/myfont16x32.otb build/myfont16x32.psf
