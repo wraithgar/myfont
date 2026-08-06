73329d6d73a806a9f6598043bbecc03466e9b71f
Gar <gar+gh@danger.computer>
2026-08-06 16:11:13 -0700

    9x16

--- convert.sh
+++ convert.sh
@@ -6,6 +6,6 @@ rm -f build/*
 bitsnpicas convertbitmap -f ttf -o build/myfont8x16.ttf build/myfont8x16.psf
 bitsnpicas convertbitmap -f otb -o build/myfont8x16.otb build/myfont8x16.psf
 
-~/Development/console-fonts-utils/psftx2psf myfont16x32.psftx build/myfont16x32.psf
-bitsnpicas convertbitmap -f ttf -o build/myfont16x32.ttf build/myfont16x32.psf
-bitsnpicas convertbitmap -f otb -o build/myfont16x32.otb build/myfont16x32.psf
+~/Development/console-fonts-utils/psftx2psf myfont9x16.psftx build/myfont9x16.psf
+bitsnpicas convertbitmap -f ttf -o build/myfont9x16.ttf build/myfont9x16.psf
+bitsnpicas convertbitmap -f otb -o build/myfont9x16.otb build/myfont9x16.psf
