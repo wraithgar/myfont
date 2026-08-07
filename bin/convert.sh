c14ad87057df1261ad3e517f46768ae0295d5c6d
Gar <gar+gh@danger.computer>
2026-08-07 14:58:11 -0700

    starting modern version
    Combine glyph and bitmap, convert to sfd, remove overlap

--- bin/convert.sh
+++ bin/convert.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 # assumes myfont.psftx is the authoritative source
 mkdir -p build
-rm -f build/*
-~/Development/console-fonts-utils/psftx2psf myfont8x16.psftx build/myfont8x16.psf
-bitsnpicas convertbitmap -f ttf -o build/myfont8x16.ttf build/myfont8x16.psf
-bitsnpicas convertbitmap -f otb -o build/myfont8x16.otb build/myfont8x16.psf
+rm -f build/myfont.*
+# ~/Development/console-fonts-utils/psftx2psf myfont8x16.psftx build/myfont8x16.psf
+# bitsnpicas convertbitmap -f ttf -o build/myfont8x16.ttf build/myfont8x16.psf
+# bitsnpicas convertbitmap -f otb -o build/myfont8x16.otb build/myfont8x16.psf
 
-~/Development/console-fonts-utils/psftx2psf myfont9x16.psftx build/myfont9x16.psf
-bitsnpicas convertbitmap -f ttf -o build/myfont9x16.ttf build/myfont9x16.psf
-bitsnpicas convertbitmap -f otb -o build/myfont9x16.otb build/myfont9x16.psf
+# ~/Development/console-fonts-utils/psftx2psf myfont9x16.psftx build/myfont9x16.psf
+# bitsnpicas convertbitmap -f ttf -o build/myfont9x16.ttf build/myfont9x16.psf
+# bitsnpicas convertbitmap -f otb -o build/myfont9x16.otb build/myfont9x16.psf
 
-~/Development/console-fonts-utils/psftx2psf myfont27x64.psftx build/myfont27x64.psf
-bitsnpicas convertbitmap -f ttf -o build/myfont27x64.ttf build/myfont27x64.psf
-bitsnpicas convertbitmap -f otb -o build/myfont27x64.otb build/myfont27x64.psf
+# ~/Development/console-fonts-utils/psftx2psf myfont27x64.psftx build/myfont27x64.psf
+# bitsnpicas convertbitmap -f ttf -o build/myfont27x64.ttf build/myfont27x64.psf
+# bitsnpicas convertbitmap -f otb -o build/myfont27x64.otb build/myfont27x64.psf
+
+~/Development/console-fonts-utils/psftx2psf myfont.psftx build/myfont.psf
+bitsnpicas convertbitmap -f ttf -o build/myfont.ttf build/myfont.psf
+bitsnpicas convertbitmap -f otb -o build/myfont.otb build/myfont.psf
